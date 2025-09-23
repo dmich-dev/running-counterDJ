563efc55ff35b7365c657f79e5794bb226ea5067
dmich-dev <michaud671468@newmanu.edu>
2025-09-22 23:16:08 -0500

    added a feature that allows you to change how the counter increases each time

--- counter.js
+++ counter.js
@@ -1,77 +1,62 @@
-class Counter {
-    private count: number;
-    private intervalId: number | null;
-    private isRunning: boolean;
-    private stepSize: number; // Add this
-
-    constructor(startValue: number = 0, stepSize: number = 1) {
+var Counter = /** @class */ (function () {
+    function Counter(startValue, stepSize) {
+        if (startValue === void 0) { startValue = 0; }
+        if (stepSize === void 0) { stepSize = 1; }
         this.count = startValue;
         this.stepSize = stepSize;
         this.intervalId = null;
         this.isRunning = false;
     }
-
-    start(): void {
+    Counter.prototype.start = function () {
+        var _this = this;
         if (this.isRunning) {
             console.log("Counter is already running!");
             return;
         }
-
         console.log("Starting counter...");
         this.isRunning = true;
-
-        this.intervalId = setInterval(() => {
-            this.count += this.stepSize; // Changed this line
-            console.log(`Counter: ${this.count}`);
+        this.intervalId = setInterval(function () {
+            _this.count += _this.stepSize; // Changed this line
+            console.log("Counter: ".concat(_this.count));
         }, 1000);
-    }
-
-    stop(): void {
+    };
+    Counter.prototype.stop = function () {
         if (!this.isRunning || !this.intervalId) {
             console.log("Counter is not running!");
             return;
         }
-
         console.log("Stopping counter...");
         clearInterval(this.intervalId);
         this.intervalId = null;
         this.isRunning = false;
-        console.log(`Final count: ${this.count}`);
-    }
-
-    reset(): void {
-        const wasRunning = this.isRunning;
+        console.log("Final count: ".concat(this.count));
+    };
+    Counter.prototype.reset = function () {
+        var wasRunning = this.isRunning;
         if (wasRunning) {
             this.stop();
         }
-
         this.count = 0;
         console.log("Counter reset to 0");
-
         if (wasRunning) {
             this.start();
         }
-    }
-
-    getCurrentCount(): number {
+    };
+    Counter.prototype.getCurrentCount = function () {
         return this.count;
-    }
-
-    isCounterRunning(): boolean {
+    };
+    Counter.prototype.isCounterRunning = function () {
         return this.isRunning;
-    }
-
-    setStepSize(step: number): void {
+    };
+    Counter.prototype.setStepSize = function (step) {
         this.stepSize = step;
-        console.log(`Step size set to: ${step}`);
-    }
-}
-
-const counter = new Counter(0, 2);
-
+        console.log("Step size set to: ".concat(step));
+    };
+    return Counter;
+}());
+var counter = new Counter(0, 2);
 // start the counter
 counter.start();
-
 // // make it stop after 10 seconds
 // setTimeout(() => {
 //     counter.stop();
